386d700491375ade3c38df9e13d655fe8ff38e14
Adrien Comte <adrien.comte@keyrus.com>
2025-12-16 09:16:10 +0000

    patch int_localbike__f_orders

--- models/intermediate/localbike/int_localbike__f_orders.sql
+++ models/intermediate/localbike/int_localbike__f_orders.sql
@@ -12,3 +12,9 @@ select
 from
     {{ ref("int_localbike__d_calendar") }} cal
     left outer join {{ ref("stg_localbike__orders") }} orders on (cal.date_day = orders.order_date)
+group by
+    date_day,
+    store_id,
+    staff_id,
+    customer_id,
+    status_id
